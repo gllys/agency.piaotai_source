3ac6acaee4505a67e8a803d839547d2763d0a1b5
gllys_letian <474040454@qq.com>
2015-04-09 17:39:13 +0800

    new banch

--- src/pagesbuilt/InterFRegister.java
+++ src/pagesbuilt/InterFRegister.java
@@ -7,27 +7,25 @@ package pagesbuilt;
  */
 public interface InterFRegister {
 
-	String local = "http://agency.test.demo.org.cn/";
-	String remote = "http://www.piaotai.com";
+	String local      			= "http://agency.test.demo.org.cn/";
+	String remote 				= "http://www.piaotai.com";
 	// REGAGENCY
-
-	String useraccount = "//input [@id ='reg_account']";
-	String userpassword = "//input [@id='reg_password']|//input[@id = 'reg_repassword']";
-	String regsubmit = "//button[@id='btn_reg']";
-	String mobile = "//input[@id='reg_mobile']";
-
+	String useraccount 			= "//input [@id ='reg_account']";
+	String userpassword 		= "//input [@id='reg_password']|//input[@id = 'reg_repassword']";
+	String regsubmit 			= "//button[@id='btn_reg']";
+	String mobile 				= "//input[@id='reg_mobile']";
 	// AHEADAGENCY
-
-	String getinreg = "//a [@class = 'new-account']";
-	String dashpicture = "//div [@class='item active']";
-	String dashtitle = "//div[@class='nav-inner']/a";
-	
-	/*********************************************************************************************************************/
-	/**
-	 * @author Gllys
-	 */
-
-	String mobilenumber = "13816219316";
-	String useraccountnumber = "gllysleletian";
-	String userpasswordnumber = "0823letian";
+	String getinreg 			= "//a [@class = 'new-account']";
+	String dashpicture 			= "//div [@class='item active']";
+	String dashtitle			= "//div[@class='nav-inner']/a";
+	String weibo 				= "//div[@class='nav-inner']/a";
+	String shouye				= "//div[@class='nav-inner']/a[1]";
+	String piaotaijieshao		= "//div[@class='nav-inner']/a[2]";
+	String ruheruzhu			= "//div[@class='nav-inner']/a[3]";
+	String peitaoshebei			= "//div[@class='nav-inner']/a[4]";		
+	String hezuozizhi			= "//div[@class='nav-inner']/a[5]";		
+	String lianxiwomen 			= "//div[@class='nav-inner']/a[6]";		
+	String mobilenumber 		= "13816219316";
+	String useraccountnumber 	= "gllysleletian";
+	String userpasswordnumber 	= "0823letian";
 }
--- testcases/loginagency/LoginAgency.java
+++ testcases/loginagency/LoginAgency.java
@@ -1,17 +1,12 @@
 package loginagency;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-
-
 import org.testng.annotations.Test;
 
 import pagesbuilt.InterFRegister;
@@ -34,25 +29,59 @@ public class LoginAgency {
 		du = new NewDo(driver);
 		wat = new wait(driver);
 		driver.manage().window().maximize();
-        
+
 	}
-          //du.whats("//div[@class='nav-inner']/a").toArray();
+
 	@Test
-	public void checkValue(){
+	public void checkValue() {
 		driver.get(InterFRegister.local);
 		wat.waitForElementPresent(InterFRegister.dashpicture);
 		int titlenum = 6;
-		int realtitlenum = Integer.valueOf(du.whats(InterFRegister.dashtitle).size());
+		int realtitlenum = Integer.valueOf(du.whats(InterFRegister.dashtitle)
+				.size());
 		Assert.assertEquals(titlenum, realtitlenum);
-	}	
-	
-	@Test
-	public void checkSix(){
-
-	    WebElement alltitlename = driver.findElement(By.xpath("//div[@class='nav-inner']//a[@href][1]"));
-		System.out.println(alltitlename);
-//		for(WebElement eachtitlename:alltitlename)
-//		   System.out.println(eachtitlename);
 	}
-	
+
+	@Test
+	public void checkSix() {
+		driver.get(InterFRegister.local);
+		wat.waitForElementPresent(InterFRegister.dashpicture);
+		List<WebElement> alltitles = du.whats(InterFRegister.dashtitle);
+		String[] titlelist = { "首页", "票台介绍", "如何入驻", "配套设备", "合作资质", "联系我们" };
+		int num = 0;
+		for (WebElement eachtitle : alltitles) {
+			Assert.assertEquals(eachtitle.getText(), titlelist[num]);
+			++num;
+		}
+	}
+
+	@Test
+	public void checkBtn() {
+		// Actions dargger = new Actions(driver);
+		driver.get(InterFRegister.local);
+		du.what(InterFRegister.shouye).click();
+		Assert.assertEquals(du.what(InterFRegister.piaotaijieshao)
+				.isDisplayed(), true);
+		
+		Assert.assertEquals(du.what(InterFRegister.ruheruzhu).isDisplayed(),
+				true);
+		Assert.assertEquals(du.what(InterFRegister.peitaoshebei).isDisplayed(),
+				true);
+		Assert.assertEquals(du.what(InterFRegister.hezuozizhi).isDisplayed(),
+				true);
+		Assert.assertEquals(du.what(InterFRegister.lianxiwomen).isDisplayed(),
+				true);
+		Assert.assertEquals(
+				du.what(InterFRegister.shouye).getLocation().getY(),
+				du.what(InterFRegister.piaotaijieshao).getLocation().getY());
+
+	}
+
+//	@Test
+//	public void checkOtaBtn(){
+//		driver.get(InterFRegister.local);
+//		du.what(InterFRegister.weibo).click();
+//		
+//		
+//	}
 }
