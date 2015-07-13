c9832c843580a83e875c0d656db28e2f909a5129
gllys_letian <474040454@qq.com>
2015-07-13 11:07:56 +0800

    3

--- testCasesAgency/registsAgency/RegAccount.java
+++ testCasesAgency/registsAgency/RegAccount.java
@@ -1,5 +1,39 @@
 package registsAgency;
 
+import java.util.Random;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import datasource.BrowsersSet;
+import datasource.BrowsersType;
+import dosource.NewDo;
+import pagesbuilt.InterFRegister;
+import pagesbuilt.SimplyHomePage;
+import pagesbuilt.SimplyRegisterOnAgency;
+import waitsource.wait;
+import errormessage.ErrorCode;
+
 public class RegAccount {
 
+	private WebDriver driver;
+	private NewDo du;
+	private wait wat;
+
+	@BeforeClass
+	public void inialize() {
+		BrowsersSet browser = new BrowsersSet(BrowsersType.chrome);
+		driver = browser.web_driver;
+		du = new NewDo(driver);
+		wat = new wait(driver);
+		driver.manage().window().maximize();
+		driver.get(InterFRegister.local);
+	}
+
+	
+	
 }
